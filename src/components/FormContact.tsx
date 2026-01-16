7b821d5c7475c1dd8170ff4d47821d87fcd211f1
CarlosTrini <carlostrinidad952@gmail.com>
2026-01-16 01:16:56 -0600

    página de contacto/reserva => formularios estilos

--- src/components/FormContact.tsx
+++ src/components/FormContact.tsx
@@ -60,12 +60,17 @@ const FormContact: React.FC<Props> = ({ typeForm }) => {
         );
     };
 
-    const ServicesSelect = (selectValueFn) => {
+    interface ServicesSelectProps {
+        value: string;
+        selectValueElement: (valueSelected: React.ChangeEvent<HTMLSelectElement>) => void;
+    }
+
+    const ServicesSelect: React.FC<ServicesSelectProps> = ({ value, selectValueElement }) => {
         return (
             <div className={`t-input input-contact`}>
                 <label htmlFor="contactEmail"><span>*</span> Selección el paquete de tu interés:</label>
 
-                <select name="servicesTypeSelect" onChange={selectValueFn}>
+                <select name="servicesTypeSelect" value={value} onChange={(e: React.ChangeEvent<HTMLSelectElement>) => { selectValueElement(e); }}>
                     <option value="wedding">Boda</option>
                     <option value="business">Empresa</option>
                     <option value="maternity">Maternidad</option>
@@ -85,12 +90,16 @@ const FormContact: React.FC<Props> = ({ typeForm }) => {
                             <label htmlFor="contactName"
                             ><span>*</span> Nombre:
                             </label>
+                            {/* <div className='form-input'> */}
                             <input
                                 onChange={handleChageInput}
                                 type="text"
                                 name="contactName"
                                 placeholder="Ingresa tu nombre"
                             />
+                            {/* <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 640 640"><path fill="#f8f7f2" d="M125.4 128C91.5 128 64 155.5 64 189.4C64 190.3 64 191.1 64.1 192L64 192L64 448C64 483.3 92.7 512 128 512L512 512C547.3 512 576 483.3 576 448L576 192L575.9 192C575.9 191.1 576 190.3 576 189.4C576 155.5 548.5 128 514.6 128L125.4 128zM528 256.3L528 448C528 456.8 520.8 464 512 464L128 464C119.2 464 112 456.8 112 448L112 256.3L266.8 373.7C298.2 397.6 341.7 397.6 373.2 373.7L528 256.3zM112 189.4C112 182 118 176 125.4 176L514.6 176C522 176 528 182 528 189.4C528 193.6 526 197.6 522.7 200.1L344.2 335.5C329.9 346.3 310.1 346.3 295.8 335.5L117.3 200.1C114 197.6 112 193.6 112 189.4z" /></svg> */}
+                            {/* </div> */}
+
                         </div>
                         <div className={`t-input input-contact`}>
                             <label htmlFor="contactEmail"
@@ -128,12 +137,12 @@ const FormContact: React.FC<Props> = ({ typeForm }) => {
                             >
                                 <option value="general">Información general</option>
                                 <option value="pack">Información de algún paquete</option>
-                                <option value="session">Cotización de sesión personalizada</option>
+                                <option value="session">Cotizar sesión personalizada</option>
                             </select>
                         </div>
                         {
                             showServices && (
-                                <ServicesSelect selectValueFn={handleChageInput} />
+                                <ServicesSelect value={formInputs['contactServicesSelect'].value} selectValueElement={handleChageInput} />
                             )
                         }
                         <ButtonsForm />
